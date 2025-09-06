94860a267fa00224a05222a05300fa44d98870b5
Aether <64797587+Aeastr@users.noreply.github.com>
2025-09-06 03:07:09 -0400

    Initial Commit

---
+++ obversable-demo/obversable_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  obversable_demoApp.swift
+//  obversable-demo
+//
+//  Created by Aether on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct obversable_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
